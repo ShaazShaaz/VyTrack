c587a16fbc9dc97c920a703a7c6c027267ac0f76
Robinson Martinez Martinez <robinsonmartinez23@hotmail.com>
2021-10-05 23:55:09 -0400

    New Common areas

--- src/test/java/pages/UsersCommonArea.java
+++ src/test/java/pages/UsersCommonArea.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-public class UserCommonArea {
+public class UsersCommonArea {
     @FindBy(xpath = "//div[@id=\"main-menu\"]/ul/li/a/span[normalize-space(.)='Fleet']")
     private WebElement fleetModule;
 
@@ -18,13 +18,22 @@ public class UserCommonArea {
     @FindBy(xpath = "//div[@id=\"main-menu\"]/ul/li/a/span[contains(text(),'System')]")
     private WebElement systemModule;
 
+    @FindBy(xpath = "//div[@id=\"main-menu\"]/ul/li/a/span[contains(text(),'Dashboard')]")
+    private WebElement dashboardModule;
+
+    @FindBy(xpath = "//div[@id=\"main-menu\"]/ul/li/a/span[contains(text(),'Marketing')]")
+    private WebElement marketingModule;
+
+    @FindBy(xpath = "//div[@id=\"main-menu\"]/ul/li/a/span[contains(text(),'Reports & Segments')]")
+    private WebElement reportsAndSegmentsModule;
+
     @FindBy(xpath = "//i[@title='Get help']")
     private WebElement oroGetHelp;
 
     @FindBy(xpath = "//h1/a[@title='Fleet Management']")
     private WebElement fleetMngHomePage;
 
-    public UserCommonArea(){
+    public UsersCommonArea(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 }
