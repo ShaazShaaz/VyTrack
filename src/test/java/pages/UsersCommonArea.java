467ba4b8d76a4ccffb3edee3e84d3ea635bc9132
Faruk Hesenjan <fh522@vt.edu>
2021-10-08 04:54:22 -0400

    Faruk-us6-Test Case 197-tested and ready

--- src/test/java/pages/UsersCommonArea.java
+++ src/test/java/pages/UsersCommonArea.java
@@ -4,9 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import utilities.Driver;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class UsersCommonArea {
@@ -20,10 +23,13 @@ public class UsersCommonArea {
     @FindBy(xpath = "//h1/a[@title='Fleet Management']")
     private WebElement fleetMngHomePage;
 
-    public List<String>modulesGetText(){
+    @FindBy(xpath = "//div[@class='container']/ul[@class='nav pull-right user-menu']/li[@id='user-menu']/ul[@class='dropdown-menu']/li[not(@class='divider' or @class='last')]")
+    List<WebElement> accountOptions;
+
+    public List<String> modulesGetText() {
         // list={"Dashboard","Fleet","Customers","Sales","Activities","Marketing","Reports & Segments","System"};
 
-        List<String> expectedList=new ArrayList<>() ;
+        List<String> expectedList = new ArrayList<>();
 
         for (WebElement eachMod : modules) {
             expectedList.add(eachMod.getText());
@@ -33,15 +39,26 @@ public class UsersCommonArea {
         return expectedList;
     }
 
+    public List<String> optionsGetText() {
 
+        List<String> expectedOptions = new ArrayList<>();
 
-    public UsersCommonArea(){
-        PageFactory.initElements(Driver.getDriver(),this);
+        for (WebElement eachOption : accountOptions) {
+
+            expectedOptions.add(eachOption.getText());
+        }
+        System.out.println(expectedOptions);
+        return expectedOptions;
     }
 
-    public static WebElement chooseModule(String module){
 
-        WebElement moduleName = Driver.getDriver().findElement(By.xpath("//div[@id=\"main-menu\"]/ul/li/a/span[contains(text(),'"+module+"')]"));
+    public UsersCommonArea() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public static WebElement chooseModule(String module) {
+
+        WebElement moduleName = Driver.getDriver().findElement(By.xpath("//div[@id=\"main-menu\"]/ul/li/a/span[contains(text(),'" + module + "')]"));
         return moduleName;
     }
 }
