349a54be30546bd5cf83776b149ca276fe713a0b
YuliiaRublenko <87611408+YuliiaRublenko@users.noreply.github.com>
2021-10-08 01:56:54 -0400

    did some fork on TE-179(user create car)

--- src/test/java/tests/us2_170_UserCreatesVehicle/TC179_CreateCar_StoreAndSalesManagers.java
+++ src/test/java/tests/us2_170_UserCreatesVehicle/TC179_CreateCar_StoreAndSalesManagers.java
@@ -1,19 +1,26 @@
 package tests.us2_170_UserCreatesVehicle;
 
+import com.github.javafaker.DateAndTime;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import pages.US2_Form_CreateCarPage;
+import sun.util.calendar.BaseCalendar;
 import utilities.TestBase;
 import utilities.VyTrackUtil;
-
 import static utilities.Driver.*;
 
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
-public class TC177_CreateCar_StoreAndSalesManagers extends TestBase {
+public class TC179_CreateCar_StoreAndSalesManagers extends TestBase {
 //    Given store/sales manager is on the homePage
 //    When user select “Vehicles” under Fleet module
 //    And user click “create car” button
@@ -47,8 +54,22 @@ public class TC177_CreateCar_StoreAndSalesManagers extends TestBase {
 
             //    When user fill out general information
             Faker faker = new Faker();
-            WebElement licencePlateBox = getDriver().findElement(By.id("#custom_entity_type_LicensePlate-uid-615cbece5ad22"));
-            licencePlateBox.sendKeys(faker.numerify("##-####"));
+
+            US2_Form_CreateCarPage form = new US2_Form_CreateCarPage();
+            form.licencePlateBox.sendKeys(faker.letterify("##-")+faker.letterify("####"));
+            form.checkboxTag("Sedan").click();
+            form.driverBox.sendKeys(faker.name().fullName());
+            form.locationBox.sendKeys(faker.address().fullAddress());
+            form.chassisNumbBox.sendKeys(faker.letterify("###")+faker.numerify("############"));
+            form.modelYearBox.sendKeys("1995");            // HOW TO USE FAKER ******?
+            form.lastOdometerBox.sendKeys(faker.numerify("###-###"));
+            form.immatriculationBox.click();
+            form.immatriculationDateSelection(19,"October","2019");
+            form.firstContractBox.click();
+            form.firstContractDateSelection(3,"February","2020");
+
+
+
 
 
 
